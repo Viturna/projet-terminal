6953d6be6a42fb546b7f86473a6a5e1882ab2cc5
LineCab <line.cabaret@mmibordeaux.com>
2024-09-25 11:52:40 +0200

    dossier4

--- Dossier4.php
+++ Dossier4.php
@@ -11,7 +11,17 @@
     <div id="progress-bar-container">
         <div id="progress-bar">0%</div>
     </div>
-        <div id="output">Dossier4</div>
+    <div id="output">
+        Dossier4
+        <p class="select">/DOC : [Article1]</p>
+        <p class="select">/DOC : [Article2]</p>
+        <p class="select">/DOC : [Article3]</p>
+
+        <div id="article-display" style="display: none;">
+            <h2 id="article-title"></h2>
+            <p id="article-content"></p>
+        </div>
+    </div>
         <div id="input-area">
             <span id="prompt">> </span>
             <input type="text" id="command-input" autofocus>
@@ -20,6 +30,7 @@
     </div>
 
     <script src="assets/js/script.js"></script>
+    <script src="assets/js/articles.js"></script>
     <script src="assets/js/barreChargement.js"></script>
 </body>
 </html>
